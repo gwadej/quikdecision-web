77a2aea1590e961d3ce7a965a59b4e52ee817c04
G. Wade Johnson <gwadej@anomaly.org>
2018-11-30 21:58:08 -0600

    Move some code to the library.

---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+extern crate hyper;
+extern crate quikdecision;
+#[macro_use]
+extern crate serde_json;
+
+use std::collections::HashMap;
+use hyper::{Body,  Response};
+
+use quikdecision::{Command, Decision, Decider};
+
+pub fn process_command(cmdres: Result<Command,String>) -> Response<Body>
+{
+    let body = match cmdres
+    {
+        Ok(cmd) => {
+            let json = match cmd.decide()
+            {
+                Decision::Text(value) => { json!({ "value": value }) },
+                Decision::LabelledText{label, value} => { json!({ "label": label, "value": value }) },
+                Decision::Num(value) => { json!({ "value": value }) },
+                Decision::AnnotatedNum{value, extra} => { json!({ "value": value, "extra": extra }) },
+                Decision::Bool(value) => { json!({ "value": value }) },
+            };
+            Body::from(json.to_string())
+        },
+        Err(msg) => {
+            Body::from(json!({ "error": msg }).to_string())
+        },
+    };
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .body(body)
+        .unwrap()
+}
+
+pub fn query_params(opt_query: Option<&str>) -> HashMap<&str,&str>
+{
+    let mut params = HashMap::new();
+    if let Some(query) = opt_query
+    {
+        for p in query.split("&")
+        {
+            let kv = p.split("=").take(2).collect::<Vec<&str>>();
+            if kv.len() == 2 { params.insert(kv[0], kv[1]); }
+        }
+    }
+    params
+}
+
+pub fn report_error(msg: &str) -> Response<Body>
+{
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .body(Body::from(json!({ "error": msg }).to_string()))
+        .unwrap()
+}
+
+pub fn percent_params(opt_query: Option<&str>) -> Result<u32, &str>
+{
+    let params = query_params(opt_query);
+    match params.get("percent")
+    {
+        Some(percent) => match percent.parse::<u32>()
+        {
+            Ok(p) => Ok(p),
+            Err(_) => Err("'percent' value must be an integer"),
+        },
+        None => Err("Missing required 'percent'."),
+    }
+}
+
+pub fn pick_params(opt_query: Option<&str>) -> Result<(i32, i32), &str>
+{
+    let params = query_params(opt_query);
+    match (params.get("low"), params.get("high"))
+    {
+        (Some(low), Some(high)) => {
+            match (low.parse::<i32>(), high.parse::<i32>())
+            {
+                (Ok(l), Ok(h)) => Ok((l, h)),
+                (Err(_), _) => Err("'low' value must be an integer"),
+                (_, Err(_)) => Err("'high' value must be an integer"),
+            }
+        },
+        (None, None) => Err("Missing required 'low' and 'high'."),
+        (None, _) => Err("Missing required 'low'."),
+        (_, None) => Err("Missing required 'high'."),
+    }
+}
+
+pub fn select_params(opt_query: Option<&str>) -> Result<Vec<String>, &str>
+{
+    let params = query_params(opt_query);
+    match params.get("strings")
+    {
+        Some(strings) => Ok(strings.split(",").map(|s| s.to_string()).collect::<Vec<String>>()),
+        None => Err("Missing required 'strings'."),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate futures;
 extern crate hyper;
-#[macro_use]
-extern crate serde_json;
 extern crate quikdecision;
+extern crate qdweb;
 
 use futures::future;
 use hyper::rt::Future;
@@ -10,9 +9,9 @@ use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 use quikdecision::{coin,dice,oracle,percent,pick,select};
-use quikdecision::{Command,Decision,Decider};
+//use quikdecision::{Command,Decision,Decider};
 
-use std::collections::HashMap;
+use qdweb::*;
 
 /// We need to return different futures depending on the route matched,
 /// and we can do that with an enum, such as `futures::Either`, or with
@@ -22,60 +21,23 @@ use std::collections::HashMap;
 /// and extend with more types. Advanced users could switch to `Either`.
 type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
-fn process_command(cmdres: Result<Command,String>, response: &mut Response<Body>)
-{
-    match cmdres
-    {
-        Ok(cmd) => {
-            let json = match cmd.decide()
-            {
-                Decision::Text(value) => { json!({ "value": value }) },
-                Decision::LabelledText{label, value} => { json!({ "label": label, "value": value }) },
-                Decision::Num(value) => { json!({ "value": value }) },
-                Decision::AnnotatedNum{value, extra} => { json!({ "value": value, "extra": extra }) },
-                Decision::Bool(value) => { json!({ "value": value }) },
-            };
-            *response.body_mut() = Body::from(json.to_string());
-        },
-        Err(msg) => {
-            *response.body_mut() = Body::from(json!({ "error": msg }).to_string());
-        },
-    }
-}
-
-fn query_params(opt_query: Option<&str>) -> HashMap<&str,&str>
-{
-    let mut params = HashMap::new();
-    if let Some(query) = opt_query
-    {
-        for p in query.split("&")
-        {
-            let kv = p.split("=").take(2).collect::<Vec<&str>>();
-            if kv.len() == 2 { params.insert(kv[0], kv[1]); }
-        }
-    }
-    params
-}
-
-fn report_error(response: &mut Response<Body>, msg: &str)
-{
-    *response.body_mut() = Body::from(json!({ "error": msg }).to_string())
-}
-
 /// This is our service handler. It receives a Request, routes on its
 /// path, and returns a Future of a Response.
 fn echo(req: Request<Body>) -> BoxFut {
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
+    let response =
+    match (req.method(), req.uri().path())
+    {
         // Serve some instructions at /
         (&Method::GET, "/") => {
-            *response.body_mut() = Body::from("Try POSTing data to /echo");
+            Response::builder()
+                .header("Content-Type", "text/html")
+                .body(Body::from("Try POSTing data to /echo"))
+                .unwrap()
         }
 
         // Flip a coin
         (&Method::GET, "/flip") => {
-            process_command(coin::command(), &mut response);
+            process_command(coin::command())
         }
 
         // Roll dice
@@ -83,22 +45,22 @@ fn echo(req: Request<Body>) -> BoxFut {
             let params = query_params(req.uri().query());
             match params.get("expr")
             {
-                Some(expr) => process_command(dice::command(expr.to_string()), &mut response),
-                None => report_error(&mut response, "Missing required 'expr'."),
+                Some(expr) => process_command(dice::command(expr.to_string())),
+                None => report_error("Missing required 'expr'."),
             }
         }
 
         // Ask the Oracle
         (&Method::GET, "/oracle") => {
-            process_command(oracle::command(), &mut response);
+            process_command(oracle::command())
         }
 
         // Percent likely
         (&Method::GET, "/likely") => {
             match percent_params(req.uri().query())
             {
-                Ok(percent) => process_command(percent::command(percent), &mut response),
-                Err(msg) => report_error(&mut response, msg),
+                Ok(percent) => process_command(percent::command(percent)),
+                Err(msg) => report_error(msg),
             }
         }
 
@@ -106,8 +68,8 @@ fn echo(req: Request<Body>) -> BoxFut {
         (&Method::GET, "/pick") => {
             match pick_params(req.uri().query())
             {
-                Ok((low, high)) => process_command(pick::command(low, high), &mut response),
-                Err(msg) => report_error(&mut response, msg),
+                Ok((low, high)) => process_command(pick::command(low, high)),
+                Err(msg) => report_error(msg),
             }
         }
 
@@ -115,61 +77,21 @@ fn echo(req: Request<Body>) -> BoxFut {
         (&Method::GET, "/select") => {
             match select_params(req.uri().query())
             {
-                Ok(strvec) => process_command(select::command(strvec), &mut response),
-                Err(msg) => report_error(&mut response, msg),
+                Ok(strvec) => process_command(select::command(strvec)),
+                Err(msg) => report_error(msg),
             }
         }
 
         // The 404 Not Found route...
         _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
+            Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from("Unknown decision command"))
+                .unwrap()
         }
     };
 
     Box::new(future::ok(response))
-}
-
-fn percent_params(opt_query: Option<&str>) -> Result<u32, &str>
-{
-    let params = query_params(opt_query);
-    match params.get("percent")
-    {
-        Some(percent) => match percent.parse::<u32>()
-        {
-            Ok(p) => Ok(p),
-            Err(_) => Err("'percent' value must be an integer"),
-        },
-        None => Err("Missing required 'percent'."),
-    }
-}
-
-fn pick_params(opt_query: Option<&str>) -> Result<(i32, i32), &str>
-{
-    let params = query_params(opt_query);
-    match (params.get("low"), params.get("high"))
-    {
-        (Some(low), Some(high)) => {
-            match (low.parse::<i32>(), high.parse::<i32>())
-            {
-                (Ok(l), Ok(h)) => Ok((l, h)),
-                (Err(_), _) => Err("'low' value must be an integer"),
-                (_, Err(_)) => Err("'high' value must be an integer"),
-            }
-        },
-        (None, None) => Err("Missing required 'low' and 'high'."),
-        (None, _) => Err("Missing required 'low'."),
-        (_, None) => Err("Missing required 'high'."),
-    }
-}
-
-fn select_params(opt_query: Option<&str>) -> Result<Vec<String>, &str>
-{
-    let params = query_params(opt_query);
-    match params.get("strings")
-    {
-        Some(strings) => Ok(strings.split(",").map(|s| s.to_string()).collect::<Vec<String>>()),
-        None => Err("Missing required 'strings'."),
-    }
 }
 
 fn main() {
